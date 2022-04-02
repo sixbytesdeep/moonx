87f49b16b3f6592eb89d83cce57722451de0f9ba
sixbytesdeep <zridka.pletenec0j@icloud.com>
2022-04-02 11:49:59 +0200

    implemented binary expressions

--- src/expressions.rs
+++ src/expressions.rs
@@ -30,3 +30,67 @@ pub struct Binary {
     pub(crate) op: Token,
     pub(crate) right: Rc<dyn Expr>,
 }
+
+impl Expr for Binary {
+    fn evaluate(&self, env: Rc<Environment>) -> Result<Value, (String, Token)> {
+        let left = self.left.evaluate(Rc::clone(&env))?;
+        let right = self.right.evaluate(Rc::clone(&env))?;
+        let token = self.op.clone();
+        match self.op.token_type {
+            TokenType::BangEqual => Ok(is_equal(left, right, true)),
+            TokenType::EqualEqual => Ok(is_equal(left, right, true)),
+            TokenType::Greater => match (left, right) {
+                (Value::Number(a), Value::Number(b)) => Ok(Value::Bool(a > b)),
+                _ => Err((String::from("Lze porovnat jen 2 cisla."), token)),
+            },
+            TokenType::GreaterEqual => match (left, right) {
+                (Value::Number(a), Value::Number(b)) => Ok(Value::Bool(a >= b)),
+                _ => Err((String::from("Lze porovnat jen 2 cisla."), token)),
+            },
+            TokenType::Less => match (left, right) {
+                (Value::Number(a), Value::Number(b)) => Ok(Value::Bool(a < b)),
+                _ => Err((String::from("Lze porovnat jen 2 cisla."), token)),
+            },
+            TokenType::LessEqual => match (left, right) {
+                (Value::Number(a), Value::Number(b)) => Ok(Value::Bool(a <= b)),
+                _ => Err((String::from("Lze porovnat jen 2 cisla."), token)),
+            },
+            TokenType::Minus => match (left, right) {
+                (Value::Number(a), Value::Number(b)) => Ok(Value::Number(a.clone() - b.clone())),
+                _ => Err((String::from("Lze odecist jen 2 cisla."), token)),
+            },
+            TokenType::Plus => match (left, right) {
+                (Value::Number(a), Value::Number(b)) => Ok(Value::Number(a.clone() + b.clone())),
+                (Value::String(a), Value::String(b)) => Ok(Value::String(format!("{}{}", a, b))),
+                _ => Err((String::from("Lze spojit/secist jen dva retezce/cisla."), token)),
+            },
+            TokenType::Slash => match (left, right) {
+                (Value::Number(a), Value::Number(b)) => {
+                    if b.clone() == 0.0 {
+                        Err((String::from("Nelze delit nulou."), token))
+                    } else {
+                        Ok(Value::Number(a.clone() / b.clone()))
+                    }
+                }
+                _ => Err((String::from("Lze delit jen 2 cisla."), token)),
+            },
+            TokenType::Star => match (left, right) {
+                (Value::Number(a), Value::Number(b)) => Ok(Value::Number(a.clone() * b.clone())),
+                _ => Err((String::from("Lze nasobit jen 2 cisla."), token)),
+            },
+            _ => Err((String::from("Neznama operace."), token)),
+        }
+    }
+
+    fn kind(&self) -> Kind {
+        Kind::Binary 
+    }
+}
+
+fn is_equal(val1: Value, val2: Value, invert: bool) -> Value {
+    if invert {
+        Value::Bool(val1 != val2)
+    } else {
+        Value::Bool(val1 == val2)
+    }
+}
